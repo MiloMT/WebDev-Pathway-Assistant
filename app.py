50cb112511f1b93242b39157f626e0e2e230e897
MiloMT <myles@mylestaylor.dev>
2023-12-06 12:48:48 +1000

    Added categories blueprint

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from blueprints.cli_bp import db_commands
-# from blueprints.users_bp import users_bp
+from blueprints.categories_bp import categories_bp
 # from blueprints.cards_bp import cards_bp
 from setup import app
 
 
 app.register_blueprint(db_commands)
-# app.register_blueprint(users_bp)
+app.register_blueprint(categories_bp)
 # app.register_blueprint(cards_bp)
 
 print(app.url_map)
---
+++ blueprints/categories_bp.py
@@ -0,0 +1,81 @@
+from flask import Blueprint, request
+# from flask_jwt_extended import jwt_required, get_jwt_identity
+from models.category import Category, CategorySchema
+from setup import db
+# from auth import authorize
+
+
+categories_bp = Blueprint("categories", __name__, url_prefix="/categories")
+
+
+# Get all categories
+@categories_bp.route("/")
+# @jwt_required()
+def all_categories():
+    
+    stmt = db.select(Category).order_by("id")
+    categories = db.session.scalars(stmt).all()
+    return CategorySchema(many=True).dump(categories)
+
+
+# Create a category
+@categories_bp.route("/", methods=["POST"])
+# @jwt_required()
+def create_card():
+    
+    category_info = CategorySchema(exclude=["id"]).load(request.json) # type: ignore
+    
+    category = Category(
+        name = category_info["name"], # type: ignore
+        description = category_info.get("description", "") # type: ignore
+    )
+    
+    db.session.add(category)
+    db.session.commit()
+    
+    return CategorySchema().dump(category), 201
+
+
+# Get a single category
+@categories_bp.route("/<int:id>")
+# @jwt_required()
+def one_category(id):
+    stmt = db.select(Category).filter_by(id = id)
+    category = db.session.scalar(stmt)
+    if category:
+        # authorize(card.user_id)
+        return CategorySchema().dump(category)
+    return {"error": "Category not found"}, 404
+
+
+# Update a single category
+@categories_bp.route("/<int:id>", methods=["PUT", "PATCH"])
+# @jwt_required()
+def update_category(id):
+    category_info = CategorySchema(exclude=["id"]).load(request.json) # type: ignore
+    
+    stmt = db.select(Category).filter_by(id = id)
+    category = db.session.scalar(stmt)
+    if category:
+        # authorize(category.user_id)
+        category.name = category_info.get("name", category.name) # type: ignore
+        category.description = category_info.get("description", category.description) # type: ignore
+        
+        db.session.commit()
+        
+        return CategorySchema().dump(category), 200
+    return {"error": "Category not found"}, 404   
+
+
+# Delete a single category
+@categories_bp.route("/<int:id>", methods=["DELETE"])
+# @jwt_required()
+def delete_category(id):
+    stmt = db.select(Category).filter_by(id = id)
+    category = db.session.scalar(stmt)
+    if category:
+        # authorize(card.user_id)
+        db.session.delete(category)
+        db.session.commit()
+        return {"status": f"{category.name} has been deleted"}, 200
+    return {"error": "Category not found"}, 404
